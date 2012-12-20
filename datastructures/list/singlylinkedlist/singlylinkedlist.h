d1eb6764a7da9879707489093d9c83e912bec434
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-12-21 01:04:17 +0200

    Filling up missing documentation for Singly Linked List class.

--- datastructures/list/singlylinkedlist/singlylinkedlist.h
+++ datastructures/list/singlylinkedlist/singlylinkedlist.h
@@ -12,17 +12,49 @@
 
 class SinglyLinkedList {
 public:
+    /**
+     * Constructor.
+     */
     SinglyLinkedList();
+
+    /**
+     * Destructor.
+     */
     virtual ~SinglyLinkedList();
 
     // -- getter methods
 
+    /**
+     * Getter for the list's head.
+     *
+     * @return The list's head.
+     */
     inline SinglyLinkedListNode* head() { return m_head; }
 
     // -- public methods
 
+    /**
+     * Prepends the node given to the list's head.
+     *
+     * @param node The list node to be prepended.
+     */
     void prepend(SinglyLinkedListNode* node);
+
+    /**
+     * Appends the node given to the list's tail.
+     *
+     * @param node The list node to be appended.
+     */
     void append(SinglyLinkedListNode* node);
+
+    /**
+     * Finds the first node in the list that matches the data given.
+     *
+     * @param data The data to base the find process.
+     *
+     * @return A pointer to the node in the list first found to contain the
+     *         data given; <code>null</code> if no such node exists.
+     */
     SinglyLinkedListNode* find(int data);
 
     /**
@@ -40,12 +72,25 @@ public:
      */
     void clear();
 
+    /**
+     * Reverts the list.
+     */
     void revert();
+
+    /**
+     * Sorts the list.
+     */
     void sort();
 
+    /**
+     * Prints the list.
+     */
     void print();
 protected:
 private:
+    /**
+     * Pointer to the list's head.
+     */
     SinglyLinkedListNode* m_head;
 };
 
