70b790816d748bc18357a38ee140406e08935075
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-12-28 12:06:57 +0200

    Adding example implementation of recursive factorial computation.

---
+++ problems/factorial.cpp
@@ -0,0 +1,31 @@
+/*
+ * factorial.cpp
+ *
+ *  Created on: Dec 28, 2012
+ *      Author: billy
+ */
+
+#include <iostream>
+
+float factorial (int number)
+{
+    if (number > 1) {
+        // Recursive case.
+        return number * factorial(number - 1);
+    }
+    else {
+        // Base case.
+        return 1;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int num = 4;
+
+    std::cout << num << "! = " << factorial(num) << std::endl;
+
+    return 0;
+}
+
+
