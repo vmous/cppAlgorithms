8390489982f4915e19222838466411a725c0bc73
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-12-29 01:52:58 +0200

    Adding recursive factorial algorithm that returns all of its intermediate results. Introducing iterative approach to simple factorial implementation.

--- problems/factorial.cpp
+++ problems/factorial.cpp
@@ -7,11 +7,11 @@
 
 #include <iostream>
 
-int factorial (int number)
+int factorial_recursive (int number)
 {
     if (number > 1) {
         // Recursive case.
-        return number * factorial(number - 1);
+        return number * factorial_recursive(number - 1);
     }
     else {
         // Base case.
@@ -19,11 +19,48 @@ int factorial (int number)
     }
 }
 
+/**
+ * Iterative implementations are in most cases well more efficient than their
+ * recursive counterparts.
+ */
+int factorial_iterative (int number)
+{
+    int val = 1;
+    for (int i = number; i > 1; i--)
+        val *= i;
+
+    return val;
+}
+
+/**
+ * For example, if you need a factorial routine that returns all of its
+ * intermediate results (factorials less than n), as well as the final result
+ * (n!), you most naturally return these results as an integer array, which
+ * means the routine needs to allocate an array. You also need to know where
+ * in the array each result should be written. These tasks are most easily
+ * accomplished using a wrapper routine, as follows:
+ */
+int factorial_recursive_all(int number, int* results, int level)
+{
+    if (number > 1) {
+        // Recursive case.
+        results[level] = number * factorial_recursive_all(number - 1, results, level + 1);
+        return results[level];
+    }
+    else {
+        // Base case.
+        results[level] = 1;
+        return 1;
+    }
+}
+
 int main(int argc, char** argv)
 {
-    int num = 4;
+    int num = 5;
 
-    std::cout << num << "! = " << factorial(num) << std::endl;
+//    std::cout << num << "! = " << factorial_recursive(num) << std::endl;
+
+    std::cout << num << "! = " << factorial_iterative(num) << std::endl;
 
     return 0;
 }
