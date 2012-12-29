061903e350a305c9cb0aa710dac7d11c2822fa6e
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-12-29 12:58:53 +0200

    Introducing recursive permutation algorithm. Old iterative approach was wrong, thus removed.

--- problems/stringpermutator.cpp
+++ problems/stringpermutator.cpp
@@ -1,30 +1,49 @@
 /*
  * stringpermutator.cpp
  *
- *  Created on: Dec 14, 2012
+ * Implement a routine that prints all possible orderings of the characters in
+ * a string. In other words, print all permutations that use all the characters
+ * from the original string. For example, given the string “hat”, your function
+ * should print the strings “tha”, “aht”, “tah”, “ath”, “hta”, and “hat”. Treat
+ * each character in the input string as a distinct character, even if it is
+ * repeated. Given the string “aaa”, your routine should print “aaa” six times.
+ * You may print the permutations in any order you choose.
+ *
+ * Created on: Dec 14, 2012
  *      Author: billy
  */
 #include <iostream>
 
-#define MAX_STRING_LENGTH 3
+#define MAX_STRING_LENGTH 4
 
-char s[MAX_STRING_LENGTH] = {'a','b','c'};
+char string[MAX_STRING_LENGTH] = {'a','b','c','d'};
 
-char tmp;
+void swap(char* x, char* y)
+{
+    char tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void permute_recursive(char *s, int i, int n)
+{
+    if (i == n) {
+        std::cout << s << std::endl;
+    }
+    else {
+        for (int j = i; j <=n; j++) {
+            swap( (s + i), (s + j) );
+            permute_recursive(s, i + 1, n);
+            // backtrack
+            swap( (s + i), (s + j) );
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
-  for (int i = 0; i < MAX_STRING_LENGTH; i++) {
-    for (int j = 0; j < MAX_STRING_LENGTH; j++) {
-       if (i != j) {
-         tmp = s[i];
-         s[i] = s[j];
-         s[j] = tmp;
-       }
-       std::cout << s << " ";
-    }
-  }
-  std::cout << std::endl;
+//    permute_iterative();
+    permute_recursive(string, 0, MAX_STRING_LENGTH - 1);
 }
 
 
