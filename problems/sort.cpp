9d7e22b217f7053a234df5faa179cfb2fe067e1b
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-01-04 23:42:02 +0200

    Implementing the selection sort algorithm.

--- problems/sort.cpp
+++ problems/sort.cpp
@@ -17,6 +17,10 @@ void print_int_array(int* arr, int n)
     std::cout << std::endl;
 }
 
+// #############################################################################
+// INSERTION SORT
+// #############################################################################
+
 /**
  * This function implements the insertion sort algorithm.
  * Worst case: Θ (n^2)
@@ -35,10 +39,36 @@ void insertion_sort(int* arr, int n)
     }
 }
 
+// #############################################################################
+// SELECTION SORT
+// #############################################################################
+
+/**
+ * This function implements the selection sort algorithm.
+ * Worst case: Θ (n^2)
+ */
+void selection_sort(int* arr, int n)
+{
+    int min;
+
+    for (int i = 0; i < n; i++) {
+        min = i;
+        for (int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+
+        int tmp = *(arr + i);
+        *(arr + i) = *(arr + min);
+        *(arr + min) = tmp;
+    }
+}
+
 int main(int argc, char** argv)
 {
     print_int_array(array, ARRAY_LENGTH);
-    insertion_sort(array, ARRAY_LENGTH);
+//    insertion_sort(array, ARRAY_LENGTH);
+    selection_sort(array, ARRAY_LENGTH);
     print_int_array(array, ARRAY_LENGTH);
 
     return 0;
