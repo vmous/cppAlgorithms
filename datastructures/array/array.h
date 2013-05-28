3e0a432e3eec31e2d9886db6a2eaac4ea15a796f
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-05-28 22:19:06 +0300

    Refactoring Array to become a template class.

--- datastructures/array/array.h
+++ datastructures/array/array.h
@@ -1,126 +1,139 @@
 #ifndef ARRAY_H_
 #define ARRAY_H_
 
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <climits>
+
 /**
- * @class Array
+ * @class Array<T>
  *
  * @file array.h
  *
- * @brief Standard C array container class definition.
+ * Standard C array container class definition.
  *
- * <p>
  * An array is the fundamental contiguously-allocated data structure. It
  * consists of fixed-size data records such that each element can be
  * efficiently located by its index. Arrays are used to construct more abstract
- * data structures such as <emph>stacks<emph>, <emph>queues</emph>,
- * <emph>associative arrays (dictionaries)</emph>.
- * </p>
+ * data structures such as <em>stacks</em>, <em>queues</em>,
+ * <em>associative arrays (dictionaries)</em>.
  *
- * <dl>
  * Advantages of contiguously-allocated structures are
- *
- * <dt>Constant-time (O(1)) access given the index</dt>
- * <dd>– Because the index of each element maps directly to a particular memory
+ * <ul>
+ * <li>
+ * Constant-time (O(1)) access given the index.<br />
+ * Because the index of each element maps directly to a particular memory
  * address, we can access arbitrary data items instantly provided we know the
- * index.</dd>
- *
- * <dt>Space efficiency</dt>
- * <dd>– Arrays consist purely of data, so no space is wasted with links or
+ * index.
+ * </li>
+ * <li>
+ * Space efficiency.<br />
+ * Arrays consist purely of data, so no space is wasted with links or
  * other formatting information. Further, end-of-record information is not
- * needed because arrays are built from fixed-size records.</dd>
- *
- * <dt>Memory locality</dt>
- * <dd>– A common programming idiom involves iterating through all the elements
+ * needed because arrays are built from fixed-size records.
+ * </li>
+ * <li>
+ * Memory locality.<br />
+ * A common programming idiom involves iterating through all the elements
  * of a data structure. Arrays are good for this because they exhibit excellent
  * memory locality. Physical continuity between successive data accesses helps
- * exploit the high-speed cache memory on modern computer architectures.</dd>
+ * exploit the high-speed cache memory on modern computer architectures.
+ * </li>
+ * </ul>
  *
- * </dl>
+ * Main disadvantage is
+ * <ul>
+ * <li>
+ * Size adjustment.<br />
+ * We cannot (easily) adjust their size during runtime.
+ * </li>
+ * </ul>
  *
- * <p>
- * The most important down-side of using arrays, is that we cannot (easily)
- * adjust their size during runtime. We can bypass this problem by exploiting
- * dynamic memory allocation for acquiring a bigger array in memory and then
- * copy the old one, to the lower half of the new one. Obviously, though, this
- * mechanism incurs additional burden, that breaks the guarantee of constant
- * time (O(1)), worst case, insertion cost. If we double the size of the array
- * each time we run out of space it can be proven (see Skiena's <i>The
- * Algorithm Design Manual</i>, 2nd edition, page 67) that after a total of n
- * insertions each of the n elements move only two times on average, and the
- * total work of managing the dynamic array is the same O(n) as it would have
- * been if a single array of sufficient size had been allocated in advance!
- * </p>
+ * Expanding arrays can be implemented by exploiting dynamic memory allocation.
+ * A bigger array can be acquired and then copy the old one to the lower half
+ * of the new one. It can be proven<sup>1</sup> that by doubling the size of
+ * the array when we need to expand, each of the <i>n</i> elements need to be
+ * moved 2 times on average. This means that the amount of management overhead
+ * incurred by this mechanism (i.e., O(n)), is the same as it would have been
+ * if a single array of sufficient size had been allocated in advance! Where we
+ * definitely loose is on insertion complexity, which becomes
+ * <em>amortized</em> O(1) because of the corner cases when an insertion causes
+ * an expansion as well.
  *
- * <p>
+ * <h3>Dictionary operations</h3>
+ *
  * The asymptotic worst-case running times for each of the seven fundamental
  * dictionary operations when the data structure is implemented as an array
  * are shown bellow:
- * <table border="0">
+ * <table border="1">
  * <tr>
- * <th>Dictionary operation</th>
+ * <th>Operation</th>
  * <th>Unsorted array</th>
  * <th>Sorted array</th>
  * </tr>
  * <tr>
- * <td>Search</td>
+ * <td><tt>Search</tt></td>
  * <td>O(n)</td>
  * <td>O(logn)</td>
  * </tr>
  * <tr>
- * <td>Insert</td>
+ * <td><tt>Insert</tt></td>
  * <td>O(1)</td>
  * <td>O(n)</td>
  * </tr>
  * <tr>
- * <td>Delete</td>
- * <td>O(1)*</td>
+ * <td><tt>Delete</tt></td>
+ * <td>O(1)<sup>*</sup></td>
  * <td>O(n)</td>
  * </tr>
  * <tr>
- * <td>Successor</td>
- * <td>O(n)</td>
- * <td>O(1)</td>
- * </tr>
- * <tr>
- * <td>Predecessor</td>
+ * <td><tt>Successor</tt></td>
  * <td>O(n)</td>
  * <td>O(1)</td>
  * </tr>
  * <tr>
- * <td>Minimum</td>
+ * <td><tt>Predecessor</tt></td>
  * <td>O(n)</td>
  * <td>O(1)</td>
  * </tr>
  * <tr>
- * <td>Maximum</td>
+ * <td><tt>Minimum</tt></td>
+ * <td>O(n)</td>
+ * <td>O(1)</td>
+ * </tr>
+ * <tr>
+ * <td><tt>Maximum</tt></td>
  * <td>O(n)</td>
  * <td>O(1)</td>
  * </tr>
  * </table>
- * <p>
+ * <sup>*</sup> When removing the element on index <tt>i</tt> we can fill
+ * the hole that it leaves by just <tt>A[i]=A[n--]</tt>.
  *
- * <p>
+ * <h3>Priority queue operations</h3>
+ *
  * The asymptotic worst-case running times for each of the three fundamental
  * priority queue operations when the data structure is implemented as an array
  * are shown bellow:
- * <table border="0">
+ * <table border="1">
  * <tr>
- * <th>Priority queue operation</th>
+ * <th>Operation</th>
  * <th>Unsorted array</th>
  * <th>Sorted array</th>
  * </tr>
  * <tr>
- * <td>Insert</td>
+ * <td><tt>Insert</tt></td>
  * <td>O(1)</td>
  * <td>O(n)</td>
  * </tr>
  * <tr>
- * <td>Find-Minimum</td>
+ * <td><tt>Find-Minimum</tt></td>
  * <td>O(1)</td>
  * <td>O(1)</td>
  * </tr>
  * <tr>
- * <td>Delete-Minimum</td>
+ * <td><tt>Delete-Minimum</tt></td>
  * <td>O(n)</td>
  * <td>O(1)</td>
  * </tr>
@@ -141,58 +154,83 @@
  * linear time on an unsorted array, and hence can be folded into the cost of
  * each deletion.
  *
- * <p>
+ * <h3>Stack operations</h3>
+ *
+ * The asymptotic worst-case running times for each of the fundamental stack
+ * operations when the data structure is implemented as an array is shown
+ * bellow:
+ * <table border="1">
+ * <tr>
+ * <th>Operation</th>
+ * <th></th>
+ * </tr>
+ * <tr>
+ * <td><tt>Push</tt></td>
+ * <td>O(1)</td>
+ * </tr>
+ * <tr>
+ * <td><tt>Pop</tt></td>
+ * <td>O(1)</td>
+ * </tr>
+ * <tr>
+ * <td><tt>Peek</tt></td>
+ * <td>O(1)</td>
+ * </tr>
+ * <tr>
+ * <td><tt>isEmpty</tt></td>
+ * <td>O(1)</td>
+ * </tr>
+ * <tr>
+ * <td><tt>isFull</tt></td>
+ * <td>O(1)</td>
+ * </tr>
+ * </table>
+ *
+ * All operations are independent of <tt>n</tt> since only the top element is accessed.
+ *
+ * <sup>1</sup> Skiena, <i>The Algorithm Design Manual</i>, 2nd edition, page 67
  *
  * @created Jan 8, 2013
  * @author Vassilis S. Moustakas <vsmoustakas@gmail.com>
  */
+template<class T>
 class Array {
 public:
-    /**
-     * Default constructor.
-     */
-    Array();
+    Array(int size = 10);
+    Array(const Array<T> & obj);
 
-    /**
-     * Constructor.
-     *
-     * @param[in] size
-     *     The size to be allocated.
-     */
-    Array(int size);
-
-    /**
-     * Destructor.
-     */
     virtual ~Array();
+
+    Array<T> & operator = (const Array<T> & obj);
+    T & operator [] (int i);
+    operator T *() const;
 
     // -- getter methods
 
-    /**
-     * Gets the pointer to the wrapped array.
-     *
-     * @return The pointer to the the wrapped array.
-     */
-    inline int* pointer() { return m_pointer; }
-
-    /**
-     * Gets the size of the wrapped array.
-     *
-     * @return The size of the wrapped array.
-     */
-    inline int size() { return m_size; }
+    inline T * pointer() const { return m_pointer; }
+    inline int size() const { return m_size; }
 
     // -- setter methods
     // -- public methods
 
     /**
-     * @brief Search for an element in the array.
+     * Prints the array in one line in standard output.
+     */
+    void print();
+
+    /**
+     * Resizes the guarded array.
      *
-     * <p>
-     * This method implements a recursive algorithm.
-     * </p>
+     * @param[in] size
+     *     The new size.
+     */
+    void resize(int size);
+
+    // -- static methods
+
+    /**
+     * Search for an element in the array (recursive approach).
      *
-     * <p>
      * A binary search is O(log(n)) because half of the search is eliminated
      * (in a sense, searched) on each iteration. This is more efficient than a
      * simple search through all the elements, which would be O(n). However, in
@@ -209,23 +247,18 @@ public:
      * @param[in] upper
      *     The index of the upper bound of the array spanning, in which the
      *     method will operate.
-     * @param[in] target The target to search for.
+     * @param[in] target
+     *     The target to search for.
      *
      * @return The index of the target within the array; -1 if it wasn't found.
      */
-    static int binary_search_recursive(int* array, int lower, int upper, int target);
+    static int binary_search_recursive(T * array, int lower, int upper, T target);
 
     /**
-     * @brief Search for an element in the array.
+     * Search for an element in the array (iterative approach).
      *
-     * <p>
-     * This method implements an iterative algorithm.
-     * </p>
-     *
-     * <p>
      * Read the documentation of the recursive implementation of this method
      * for an asymptotic analysis.
-     * </p>
      *
      * @param[in] array
      *     A pointer to the array to be searched.
@@ -236,45 +269,41 @@ public:
      *
      * @return The index of the target within the array; -1 if it wasn't found.
      */
-    static int binary_search_iterative(int* array, int size, int target);
+    static int binary_search_iterative(T * array, int size, T target);
 
     /**
-     * @brief Insertion sort.
+     * Insertion sort.
      *
-     * Worst case time complexity is Θ(n^2).
+     * Worst case time complexity is Θ(n<sup>2</sup>).
      *
      * @param[in] a
      *     A pointer to the array to be sorted.
      * @param[in] n
      *     The size of the array to be sorted.
      */
-    static void insertion_sort(int* a, int n);
+    static void insertion_sort(T * a, int n);
 
     /**
-     * @brief Selection sort.
+     * Selection sort.
      *
-     * Worst case time complexity is Θ(n^2).
+     * Worst case time complexity is Θ(n<sup>2</sup>).
      *
      * @param[in] a
      *     A pointer to the array to be sorted.
      * @param[in] n
      *     The size of the array to be sorted.
      */
-    static void selection_sort(int* a, int n);
+    static void selection_sort(T * a, int n);
 
     /**
-     * @brief Merge short
+     * Merge short.
      *
-     * <p>
      * Implements a recursive algorithm.
-     * </p>
      *
-     * <p>
      * Worst case time complexity is Θ(nlog(n)). As opposed to other in-place
-     * algorithms, marger sort needs O(n) space to perform the merging step.
+     * algorithms, marge sort needs O(n) space to perform the merging step.
      * On the other hand, it is a stable sort and it can be modified to
      * implement external sorting for big data sets that do not fit in memory.
-     * </p>
      *
      * @param[in] a
      *     A pointer to the array to be sorted.
@@ -283,22 +312,10 @@ public:
      * @param[in] high
      *     .
      */
-    static void merge_sort(int* a, int low, int high);
+    static void merge_sort(T * a, int low, int high);
 
-    /**
-     * Prints the array in one line in standard output.
-     */
-    void print();
-protected:
 private:
-    /**
-     * The pointer to the array.
-     */
-    int* m_pointer;
-
-    /**
-     * The size of the array.
-     */
+    T * m_pointer;
     int m_size;
 
     /**
@@ -319,7 +336,312 @@ private:
      *
      * @see Array::merge_sort(int* a, int low, int high)
      */
-    static void merge(int* a, int l, int m, int h);
+    static void merge(T * a, int l, int m, int h);
 };
+
+template<class T>
+Array<T>::Array(int size)
+{
+    if (size < 0)
+    {
+        std::cerr << "Invalid array size!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    m_size = size;
+    m_pointer = new T[m_size];
+    if (!m_pointer)
+    {
+        std::cerr << "Memory allocation error!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+template<class T>
+Array<T>::Array(const Array<T> & obj)
+{
+    m_size = obj.size();
+    m_pointer = new T[m_size];
+
+    if (!m_pointer)
+    {
+        std::cerr << "Memory allocation error!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    T * src = obj.pointer();
+    T * dst = m_pointer;
+    memcpy(dst, src, m_size * sizeof(T));
+}
+
+
+template<class T>
+Array<T>::~Array()
+{
+    delete[] m_pointer;
+}
+
+
+template<class T>
+Array<T> & Array<T>::operator = (const Array<T> & obj)
+{
+    if (this != &obj)
+    {
+        delete m_pointer;
+        m_pointer = 0;
+        m_size = obj.size();
+        m_pointer = new T[m_size];
+
+        if (!m_pointer)
+        {
+            std::cerr << "Memory allocation error!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        T * src = obj.pointer();
+        T * dst = m_pointer;
+        memcpy(dst, src, m_size * sizeof(T));
+    }
+}
+
+
+template<class T>
+T & Array<T>::operator [] (int i)
+{
+    if (i < 0 || i >= m_size)
+    {
+        std::cerr << "Index out of range!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return m_pointer[i];
+}
+
+
+template<class T>
+Array<T>::operator T * () const
+{
+    return m_pointer;
+}
+
+
+template<class T>
+void Array<T>::print()
+{
+    for (int i = 0; i < m_size; i++)
+        std::cout << m_pointer[i] << " ";
+
+    std::cout << std::endl;
+}
+
+
+template<class T>
+void Array<T>::resize(int size)
+{
+    if (size <= m_size)
+        std::cout << "You can only augment the array." << std::endl;
+    else
+    {
+        if (size < 0)
+        {
+            std::cerr << "Invalid array size!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        T * src = m_pointer;
+        T * dst = new T[size];
+
+        if (!dst)
+        {
+            std::cerr << "Memory allocation error!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        memcpy(dst, src, m_size * sizeof(T));
+
+        delete [] m_pointer;
+
+        m_pointer = dst;
+        m_size = size;
+    }
+}
+
+template<class T>
+int Array<T>::binary_search_recursive(T * array, int lower, int upper, T target)
+{
+    int range = upper - lower;
+
+    if (range < 0) {
+        // Limits reversed.
+        return -1;
+    }
+    else if( (range == 0) && (array[lower] != target) ) {
+        // Not in array.
+        return -1;
+    }
+
+    if (array[lower] > array[upper]) {
+        // Array unordered.
+        return -1;
+    }
+
+    int center = lower + (range/2);
+
+    if (target == array[center]) {
+        // Base case.
+        return center;
+    }
+    else if (target < array[center]) {
+        return binary_search_recursive(array, lower, center - 1, target);
+    }
+    else {
+        return binary_search_recursive(array, center + 1, upper, target);
+    }
+}
+
+
+template<class T>
+int Array<T>::binary_search_iterative(T * array, int size, T target)
+{
+    int lower = 0;
+    int upper = size - 1;
+    int center;
+    int range;
+
+    while( true ) {
+
+        if( lower > upper ) {
+            // Limits reversed.
+            return -1;
+        }
+
+        range = upper - lower;
+
+        if( range == 0 && array[lower] != target ) {
+            // Not in array.
+            return -1;
+        }
+
+        if( array[lower] > array[upper] ) {
+            // Array unordered.
+            return -1;
+        }
+
+        center = lower + (range/2);
+
+        if( target == array[center] ){
+            return center;
+        }
+        else if( target < array[center] ){
+            upper = center - 1;
+        }
+        else {
+            lower = center + 1;
+        }
+    }
+
+    return -1;
+}
+
+
+template<class T>
+void Array<T>::insertion_sort(T * a, int n)
+{
+    for (int i = 1; i < n; i++) {
+        T key = *(a + i);
+
+        int j = i - 1;
+        // While we still are in bounds and we haven't found our proper place
+        // in sorted order.
+        while (i > 0 && j >= 0 && *(a + j) > key) {
+
+            *(a + j + 1) = *(a + j);
+
+            j = j - 1;
+        }
+
+        *(a + j + 1) = key;
+    }
+}
+
+
+template<class T>
+void Array<T>::selection_sort(T * a, int n)
+{
+    int min;
+
+    for (int i = 0; i < n; i++) {
+        min = i;
+        for (int j = i; j < n; j++) {
+            if(a[j] < a[min])
+                min = j;
+        }
+
+        T tmp = *(a + i);
+        *(a + i) = *(a + min);
+        *(a + min) = tmp;
+    }
+}
+
+
+template<class T>
+void Array<T>::merge_sort(T * a, int low, int high)
+{
+    int mid;
+    if (low < high) {
+        mid = (low + high)/2;
+        // Divide and conquer.
+        merge_sort(a, low, mid);
+        merge_sort(a, mid + 1, high);
+        // Combine.
+        merge(a, low, mid, high);
+    }
+}
+
+
+template<class T>
+void Array<T>::merge(T * a, int l, int m, int h)
+{
+    int n_1 = m - l + 1;
+    int n_2 = h - m;
+    int i, j, k;
+    // Two temporary arrays to hold the two arrays to be merged.
+    T * f = new T[h + 1];
+    T * x = new T[h + 1];
+
+    // Create arrays F[1..n_1 + 1] and X[1..n_2 + 1]
+    for (i = 0; i < n_1; i++) {
+        *(f + i) = *(a + l + i);
+    }
+
+    for (j = 0; j < n_2; j++) {
+        *(x + j) = *(a + m + j + 1);
+    }
+
+    // Mark the end of each temporary array using <climits> definitions.
+    // TODO: I hate this part of the algorithm! Need to re-think alternatives.
+    // TODO: This point here DOES NOT WORK for T other than int!!!
+
+    *(f + i) = INT_MAX;
+    *(x + j) = INT_MAX;
+
+    i = 0;
+    j = 0;
+    for (k = l; k <= h; k++) {
+        if ( *(f + i) <= *(x + j) ) {
+            *(a + k) = *(f + i);
+            i++;
+        }
+        else {
+            *(a + k) = *(x + j);
+            j++;
+        }
+    }
+
+    delete[] f;
+    delete[] x;
+}
+
 
 #endif /* ARRAY_H_ */
--- datastructures/array/array_test.cpp
+++ datastructures/array/array_test.cpp
@@ -10,120 +10,183 @@
  */
 
 #include <iostream>
+#include <stdlib.h>
 
 #include "array.h"
 
-void test_binary_search_recursive();
-void test_binary_search_iterative();
-void test_insertion_sort();
-void test_selection_sort();
-void test_merge_sort();
+#define MAX_RANDOM_NUMBER 1000
+
+
+void prepare_array(Array<int> & arr)
+{
+    arr[0] = 10;
+    arr[1] = 28;
+    arr[2] = 103;
+    arr[3] = 21;
+    arr[4] = 7;
+    arr[5] = 9;
+    arr[6] = 1000;
+    arr[7] = 99;
+    arr[8] = 459;
+    arr[9] = 1;
+}
+
+
+void test_copy_constructor()
+{
+    std::cout << "Testing copy constructor." << std::endl;
+
+    Array<int> foo(10);
+    prepare_array(foo);
+
+    Array<int> first(foo);
+    first.print();
+
+    Array<int> second(10);
+    second.print();
+
+    Array<int> third(first);
+    third.print();
+
+    std::cout << std::endl;
+}
+
+
+void test_resize()
+{
+    std::cout << "Testing array resize." << std::endl;
+
+    Array<int> foo(10);
+    prepare_array(foo);
+    foo.print();
+
+    foo.resize(5);
+    foo.resize(15);
+    foo.print();
+
+    std::cout << std::endl;
+}
+
+
+void test_binary_search_recursive()
+{
+    std::cout << "Testing recursive binary search." << std::endl;
+
+    Array<int> tester;
+    prepare_array(tester);
+
+    // Binary search requires sorted array.
+    Array<int>::merge_sort(tester, 0, tester.size() - 1);
+    tester.print();
+
+    int ret;
+    int num;
+
+    num = 10;
+    ret = Array<int>::binary_search_recursive(tester, 0, tester.size() - 1, num);
+    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
+
+    num = 0;
+    ret = Array<int>::binary_search_recursive(tester, 0, tester.size() - 1, num);
+    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
+
+    num = 1000000001;
+    ret = Array<int>::binary_search_recursive(tester, 0, tester.size() - 1, num);
+    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
+
+    num = 1;
+    ret = Array<int>::binary_search_recursive(tester, 0, tester.size() - 1, num);
+    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
+
+    std::cout << std::endl;
+}
+
+
+void test_binary_search_iterative()
+{
+    std::cout << "Testing iterative binary search." << std::endl;
+
+    Array<int> tester;
+    prepare_array(tester);
+
+    // Binary search requires sorted array.
+    Array<int>::merge_sort(tester, 0, tester.size() - 1);
+    tester.print();
+
+    int ret;
+    int num;
+
+    num = 10;
+    ret = Array<int>::binary_search_iterative(tester, tester.size(), num);
+    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
+
+    num = 0;
+    ret = Array<int>::binary_search_iterative(tester, tester.size(), num);
+    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
+
+    num = 1000000001;
+    ret = Array<int>::binary_search_iterative(tester, tester.size(), num);
+    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
+
+    num = 1;
+    ret = Array<int>::binary_search_iterative(tester, tester.size(), num);
+    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
+
+    std::cout << std::endl;
+}
+
+
+void test_insertion_sort()
+{
+    std::cout << "Testing insertion sort." << std::endl;
+
+    Array<int> tester;
+    prepare_array(tester);
+    tester.print();
+    Array<int>::insertion_sort(tester, tester.size());
+    tester.print();
+
+    std::cout << std::endl;
+}
+
+
+void test_selection_sort()
+{
+    std::cout << "Testing selection sort." << std::endl;
+
+    Array<int> tester;
+    prepare_array(tester);
+    tester.print();
+    Array<int>::selection_sort(tester, tester.size());
+    tester.print();
+
+    std::cout << std::endl;
+}
+
+
+void test_merge_sort()
+{
+    std::cout << "Testing merge sort." << std::endl;
+
+    Array<int> tester;
+    prepare_array(tester);
+    tester.print();
+    Array<int>::merge_sort(tester, 0, tester.size() - 1);
+    tester.print();
+
+    std::cout << std::endl;
+}
+
 
 int main(int argc, char** argv)
 {
+    test_copy_constructor();
+    test_resize();
     test_binary_search_recursive();
     test_binary_search_iterative();
     test_insertion_sort();
     test_selection_sort();
     test_merge_sort();
 
-    return 0;
-}
-
-void test_binary_search_recursive()
-{
-    std::cout << "Testing recursive binary search." << std::endl;
-
-    Array* tester = new Array();
-
-    // Binary search requires sorted array.
-    Array::merge_sort(tester->pointer(), 0, tester->size() - 1);
-
-    int ret;
-    int num;
-
-    num = 10;
-    ret = Array::binary_search_recursive(tester->pointer(), 0, tester->size() - 1, num);
-    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
-
-    num = 0;
-    ret = Array::binary_search_recursive(tester->pointer(), 0, tester->size() - 1, num);
-    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
-
-    num = 1000000001;
-    ret = Array::binary_search_recursive(tester->pointer(), 0, tester->size() - 1, num);
-    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
-
-    num = 1;
-    ret = Array::binary_search_recursive(tester->pointer(), 0, tester->size() - 1, num);
-    std::cout << "Searching " << num << " (recursive): " << ret << std::endl;
-
-    std::cout << std::endl;
-}
-
-void test_binary_search_iterative()
-{
-    std::cout << "Testing iterative binary search." << std::endl;
-
-    Array* tester = new Array();
-
-    // Binary search requires sorted array.
-    Array::merge_sort(tester->pointer(), 0, tester->size() - 1);
-
-    int ret;
-    int num;
-
-    num = 10;
-    ret = Array::binary_search_iterative(tester->pointer(), tester->size(), num);
-    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
-
-    num = 0;
-    ret = Array::binary_search_iterative(tester->pointer(), tester->size(), num);
-    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
-
-    num = 1000000001;
-    ret = Array::binary_search_iterative(tester->pointer(), tester->size(), num);
-    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
-
-    num = 1;
-    ret = Array::binary_search_iterative(tester->pointer(), tester->size(), num);
-    std::cout << "Searching " << num << " (iterative): " << ret << std::endl;
-
-    std::cout << std::endl;
-}
-
-void test_insertion_sort()
-{
-    std::cout << "Testing insertion sort." << std::endl;
-
-    Array* tester = new Array();
-    tester->print();
-    Array::insertion_sort(tester->pointer(), tester->size());
-    tester->print();
-
-    std::cout << std::endl;
-}
-
-void test_selection_sort()
-{
-    std::cout << "Testing selection sort." << std::endl;
-
-    Array* tester = new Array();
-    tester->print();
-    Array::selection_sort(tester->pointer(), tester->size());
-    tester->print();
-
-    std::cout << std::endl;
-}
-
-void test_merge_sort()
-{
-    std::cout << "Testing merge sort." << std::endl;
-
-    Array* tester = new Array();
-    tester->print();
-    Array::merge_sort(tester->pointer(), 0, tester->size() - 1);
-    tester->print();
-
-    std::cout << std::endl;
+    return EXIT_SUCCESS;
 }
